b8791f71f7ad590b5fde2637cdeaf1a588b0b15b
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-07-01 12:58:01 -0300

    Adicionando script para executar programa brModelo

---
+++ Outros/Executar_brModelo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script para executar o programa Java "brModelo"
+
+java -jar brModelo.jar
+
+# OBS: Para executar o script, antes execute o comando no terminal: "chmod +x Executar_brModelo.sh" para torna um executavel.
+
