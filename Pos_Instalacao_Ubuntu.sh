7004f12541cb9daab7e17500f627a2ad3a04c79f
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-07-08 18:37:58 -0300

    Adicionando script de pos instalaçao para o ubuntu

---
+++ Pos_Instalacao_Ubuntu.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Script de automatização para baixa os aplicativos apos a intalação para o Ubuntu 24.04 LTS.
+# Feito por d4nkali - 2024
+# OBS: Ja ter o snap e flatpak instalado no sistema.
+
+pacotes_apt = (
+
+    vlc
+    git
+    lua5.4
+    neofetch
+    gimp
+    nodejs
+    htop
+    nvtop
+    golang-go
+    default-jre
+    default-jdk
+    python3.9
+    audacity
+    nano
+    vim
+    transmission
+    gparted
+    mysql-server
+    dotnet-runtime-8.0
+    dotnet-sdk-8.0
+    p7zip
+    gufw
+
+)
+
+sudo dpkg --add-architecture i386
+
+funçao_apt(){
+
+    sudo apt update && sudo apt dist-upgrade -y
+
+    for pacote in "${pacotes_apt[@]}"; 
+    do
+
+        sudo apt install "$pacote" -y
+
+    done
+
+    sudo apt install -f
+    sudo apt autoremove
+
+}
+
+funçao_apt
+
