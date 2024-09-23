dc1f57dff947203c5db4cde888e46e844b917a88
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-23 14:38:35 -0300

    Adicionando Script para ligar ou desligar ssh no termux

---
+++ Ligar_Desligar_SSH_Termux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Script de ligar ou desligar o SSH para o Termux (Android).
+# Feito por d4nkali - 2024
+
+echo "Script de Ligar ou Desligar o SSH no Termux."
+
+sleep 1
+
+echo "Deseja saber se o SSH está funcionando? (Y/n)"
+
+read resposta_status
+
+if [ "$resposta_status" == "n" ]; then
+
+    echo "Não mostrar"
+
+else
+
+    ps aux | grep sshd
+
+fi
+
+echo "Qual ação deseja fazer?"
+echo "(Ligar [1] | Desligar [2] | Cancelar [Qualquer tecla])"
+read resposta_main
+
+if [ "$resposta_main" == "1" ]; then
+
+    echo "Ligando o SSH..."
+    sshd
+    echo "SSH ligado."
+
+elif [ "$resposta_main" == "2" ]; then
+
+    echo "Desligando o SSH..."
+    pkill sshd
+    echo "SSH desligado."
+
+else
+
+    echo "Cancelando"
+
+fi
